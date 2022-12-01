93446df9e4e0ae41eb8c859b621bcebb41142e10
Stepanova Daria <videotakvideo@gmail.com>
2022-12-01 17:26:07 +0700

    for dict challenge, exercise 1-3

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -12,7 +12,18 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
+
+names = []
+for student in students:
+    for key, value in student.items():
+        names.append(value)
+
+result = {}
+for name in names:
+    result[name] = names.count(name)
+
+for key, value in result.items():
+    print(f"{key}:{value}")
 
 
 # Задание 2
@@ -26,8 +37,18 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
+names = []
+for student in students:
+    for key, value in student.items():
+        names.append(value)
 
+result = {}
+for name in names:
+    result[name] = names.count(name)
+
+import operator
+name_repeat = max(result.items(), key=operator.itemgetter(1))[0]
+print(f"Самое частое имя среди учеников: {name_repeat}")
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -51,13 +72,58 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+class_first = school_students[0]
+class_second = school_students[1]
+class_third = school_students[2]
+
+#FIRST CLASS
+names_class_first = []
+for student in class_first:
+    for key, value in student.items():
+        names_class_first.append(value)
+
+result = {}
+for name in names_class_first:
+    result[name] = names_class_first.count(name)
+
+import operator
+name_repeat = max(result.items(), key=operator.itemgetter(1))[0]
+print(f"Самое частое имя среди учеников в классе 1: {name_repeat}")
+
+#SECOND CLASS
+names_class_second = []
+for student in class_second:
+    for key, value in student.items():
+        names_class_second.append(value)
+
+result = {}
+for name in names_class_second:
+    result[name] = names_class_second.count(name)
+
+import operator
+name_repeat = max(result.items(), key=operator.itemgetter(1))[0]
+print(f"Самое частое имя среди учеников в классе 2: {name_repeat}")
+
+#SECOND CLASS
+names_class_third = []
+for student in class_third:
+    for key, value in student.items():
+        names_class_third.append(value)
+
+result = {}
+for name in names_class_third:
+    result[name] = names_class_third.count(name)
+
+import operator
+name_repeat = max(result.items(), key=operator.itemgetter(1))[0]
+print(f"Самое частое имя среди учеников в классе 3: {name_repeat}")
+
 
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
-# Класс 2a: девочки 2, мальчики 0 
+# Класс 2a: девочки 2, мальчики 0
 # Класс 2б: девочки 0, мальчики 2
 
 school = [
